ae59d1b5f3190294c9e174e2cd9eb4aa5d111832
zhengjianliu <comezheng@gmail.com>
2020-11-01 19:13:49 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,27 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state={
+    facing: true
+  }
+
+  clickHandler = e =>{
+    this.setState({facing: !this.state.facing})
+  }
   render() {
     return (
-      <Card>
+      <Card onClick={()=>this.clickHandler()}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.facing? this.props.pokemon.sprites.front : this.props.pokemon.sprites.back} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              <p>{this.props.pokemon.hp} hp</p>
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,41 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+  state={
+    pokemons:[],
+    find: ""
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(resp=>resp.json())
+    .then(data=>{
+      this.setState({pokemons:data})
+    })
+  }
+
+  searchChangeHandler = e =>{
+    this.setState({find: e.target.value})
+  }
+
+  filteredPokemon = () =>{
+    return this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.find))
+  }
+
+  createPokemon = pokemon =>{
+    this.setState({ pokemons: [...this.state.pokemons, pokemon]})
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm createPokemon={this.createPokemon}/>
         <br />
-        <Search />
+        <Search searchChangeHandler={this.searchChangeHandler}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.filteredPokemon()}/>
       </Container>
     )
   }
